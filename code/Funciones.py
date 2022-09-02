6780018ea891163534c4543db098e7073bc0a2b4
Uriel Coria <urielcdj@gmail.com>
2022-09-02 11:46:24 -0500

    Ajustes en código
    Ajustes en el archivo Funciones

--- code/Funciones.py
+++ code/Funciones.py
@@ -1,26 +1,11 @@
 def Cuadro_Dickey_Fuller(data):
-    n = []
-    c = []
-    ct = []
-    ctt = []
-
-    types_ADF = ['n','c','ct','ctt']
     from statsmodels.tsa.stattools import adfuller
-
-    for t in types_ADF:
-        for item in data.columns:
-            if t == 'n':
-                n.append(adfuller(data[item], regression=t)[1])
-            elif t == 'c':
-                c.append(adfuller(data[item], regression=t)[1])
-            elif t == 'ct':
-                ct.append(adfuller(data[item], regression=t)[1])
-            elif t == 'ctt':
-                ctt.append(adfuller(data[item], regression=t)[1])
-
     from pandas import DataFrame
+    types_ADF = ['n','c','ct','ctt']
 
-    return DataFrame(columns=types_ADF, index=data.columns, data=list(zip(n,c,ct,ctt)))
+    return DataFrame(
+        [[adfuller(data[item], regression=t)[1] for t in types_ADF ] for item in data.columns ],
+        index=data.columns, columns=types_ADF)
 
 
 def select_order_akaike(data, AR=1, MA=0, Exog=None, Trend='n'):
@@ -63,3 +48,24 @@ def select_order_bayes(data, AR=1, MA=0, Exog=None, Trend='n'):
         index=plabels, columns=qlabels)
 
         return BIC.style.highlight_min(axis=None)
+
+
+def OLS_residual_diagnostic(Model, Lags=None):
+    import pandas as pd
+    from statsmodels.stats.diagnostic import het_arch
+    from statsmodels.stats.diagnostic import het_breuschpagan
+    from statsmodels.stats.diagnostic import het_goldfeldquandt
+    from statsmodels.stats.diagnostic import het_white
+    from statsmodels.stats.diagnostic import acorr_breusch_godfrey
+
+    tests = ['Acorr Breusch-Godfrey', 'Het Breusch-Pagan-Godfrey', 'Het Goldfeld-Quandt', 'Het White']
+    values = ['Valor Lagrange', 'Valor-P Lagrange', 'Valor F', 'Valor-P F']
+
+    data = [
+                list(acorr_breusch_godfrey(Model, nlags=Lags)),
+                list(het_breuschpagan(Model.resid, Model.model.exog)),
+                ['-', '-', het_goldfeldquandt(Model.resid, Model.model.exog)[0], het_goldfeldquandt(Model.resid, Model.model.exog)[1]],
+                list(het_white(Model.resid, Model.model.exog))
+            ]
+
+    return pd.DataFrame(index=tests, columns=values, data=data)
